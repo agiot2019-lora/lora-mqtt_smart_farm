7bb98ad742f387a74049d805164c4448dde880d4
aaa <yhye1997@gmail.com>
2019-12-10 13:55:00 -0500

    soil_moisture graph succeed

--- routes/farm.js
+++ routes/farm.js
@@ -46,6 +46,7 @@ function get_sensor_info(farm_id, cb) {
 
 //get soil moisture info from farm_id
 function get_soil_moisture_info(farm_id, cb) {
+    console.log("farm ID : ", farm_id);
     var soil_moisture = new Array();
     var sqlquery = "SELECT  * FROM soil_moisture WHERE farm_id = ?";
     connection.query(sqlquery, farm_id, function (err, rows) {
@@ -59,13 +60,23 @@ function get_soil_moisture_info(farm_id, cb) {
         } else {
             console.log("found soil_moisture array");
             if(rows.length!=0){
-            for (var i=0; i<rows.length; i++){
+                if(rows.length>6){
+            for (var i=rows.length-7; i<rows.length; i++){
                 var s_moisture = {
                     soil_moisture : rows[i].soil_moisture,
                     s_datetime : rows[i].s_datetime
                 }
                 soil_moisture.push(s_moisture);
             }
+                }else{
+                for (var i=0; i<rows.length; i++){
+                    var s_moisture = {
+                        soil_moisture : rows[i].soil_moisture,
+                        s_datetime : rows[i].s_datetime
+                    }
+                    soil_moisture.push(s_moisture);
+                }
+                }
             }else{
                 var s_moisture = {
                     soil_moisture : "",
@@ -119,8 +130,8 @@ router.get('/:farm_id', function (req, res, next) {
     console.log("farm_id : ",farm_id);
     get_farm_info(farm_id, function (result, myfarm, sensor_info, soil_moisture) {
         if (result == true) {
-            console.log("myfarm: ", myfarm);
-            console.log("sensor_info: ", sensor_info);
+            //console.log("myfarm: ", myfarm);
+            //console.log("sensor_info: ", sensor_info);
             console.log("soil_moisture : ", soil_moisture);
             res.render('index', {
                 user_id: user_id,
@@ -143,6 +154,22 @@ router.get('/create/form', function (req, res, next) {
         user_id: user_id,
     });
 });
+
+// get soil_moisture json
+router.post('/get/soil/moisture', function (req, res, next) {
+    console.log("get soil_moisture_array!")
+    var farm_id=req.body.farm_id;
+    get_soil_moisture_info(farm_id,function(result, soil_moisture){
+        if(result==true){
+            res.jsonp({success : true, soil_moisture: soil_moisture})
+        }else{
+            res.render('soil_moisture', {
+                soil_moisture:soil_moisture
+            });
+        }
+    })
+});
+
 
 // insert farm info to db
 router.post('/farm_form', function (req, res, next) {
